93e9f8b95e4b61f58f1b093eacb63cdd495050f9
KudryavtsevaSI <kudryavtseva.sofi@yandex.ru>
2025-02-11 18:47:19 +0400

    прапрап

--- ЛР5/index5.js
+++ ЛР5/index5.js
@@ -17,22 +17,42 @@ class studentClass {
     }
     info () {
             console.log('Имя: ' + this.firstName + this.secondName + ', оценки: ' + JSON.stringify(this.marks))
-    }    
+    }  
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+      }
+    set fullName(value) {
+    [this.name, this.surname] = value.split(" ");
+    }
 }
 
 class marksClass {
     constructor (subjekt, mark) {
         this.subjekt = subjekt;
-        this.mark = mark;
-        function avgMarks () {
-            let sum = 0;
-            for (let i=0; i<this.marks.length; i++) {
-                sum += this.marks[i].mark;
-                avg = sum/this.marks.length;
-            }
-            console.log(avg)
-        }
+        this.mark = mark; 
+    }
+    get subjektMarks() {
+        return `${this.subjekt} ${this.mark}`;
+    }
+    set subjektMarks(value) {
+    [this.subjekt, this.mark] = value.split(" ");
     }
 }
+
+let subjekt = new marksClass.subjekt(prompt("Введите предмет"))
+let mark = new marksClass.mark(prompt("Введите оценку"))
+console.log(marksClass)
+
+// marksClass.avgMarks = function  () {
+//     let sum = 0;
+//     for (let i=0; i<Object.keys(marksClass).length; i++) {
+//         sum += Object.keys(marksClass)[i];
+//         avg = sum/Object.keys(marksClass).length;
+//     }
+//     console.log(avg)
+//     console.log(sum)
+// }
+
+// console.log(marksClass.avgMarks())
 
 
