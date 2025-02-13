41404a22e7704e5e35e48ebedb40611c347b61b7
KudryavtsevaSI <kudryavtseva.sofi@yandex.ru>
2025-02-13 18:53:13 +0400

    Доделана ЛР 5

--- ЛР5/index5.js
+++ ЛР5/index5.js
@@ -7,24 +7,6 @@ const studentVar = {
         {subjekt: 'физика', mark: 3}]
 }
 
-function printStudentInfo(studentVar) {
-    console.log(`Студент: ${studentVar.firstName} ${studentVar.secondName}`);
-    console.log("Оценки:");
-    studentVar.marks.forEach(mark => {
-      console.log(`${mark.subjekt}: ${mark.mark}`);
-    });
-  }
-  
-  printStudentInfo(studentVar);
-
-class studentClass {
-    constructor (firstName, secondName) {
-        this.firstName = firstName;
-        this.secondName = secondName;
-        this.marks = [];  
-    } 
-}
-
 class marksClass {
     constructor (subjekt, mark) {
         this.subjekt = subjekt;
@@ -32,49 +14,84 @@ class marksClass {
     }
 }
 
+class studentClass {
+    constructor (firstName, secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.marks = [];  
+    } 
+    
+    getAverageMark() { //3. метод, возвращающий среднюю оценку студента по всем предметам
+        if (this.marks.length === 0) {
+        return 0;
+        }
+        const totalMarks = this.marks.reduce((sum, mark) => sum + mark.mark, 0);
+        let resAvgMarks = totalMarks / this.marks.length;
+        console.log(`Средняя оценка по всем предметам:  ` + resAvgMarks);
+    }
+
+    alertSubjekt1 (){ //alert для ввода предмета
+        let subjekt = prompt("Введите название предмета:");
+        this.getMarksBySubjekt(subjekt);
+    } 
+
+    getMarksBySubjekt(subjekt) { //4. метод, возвращающий все оценки по переданному предмету
+        console.log(this.marks.filter(mark => mark.subjekt === subjekt));
+    }
+
+    alertAddMark (){ //alert для добавления оценки по предмету
+        let subjekt = prompt("Введите название предмета:");
+        let mark = parseInt(prompt("Введите оценку:"));
+        this.addMark(subjekt, mark);
+    }
+
+    addMark (subjekt, mark){ //5. метод добавления оценки по предмету
+        let newMark = new marksClass(subjekt, mark);
+        this.marks.push(newMark);
+    }
+
+    alertSubjekt2 (){ //alert для ввода предмета
+        let subjekt = prompt("Введите название предмета:");
+        this.removeMarksBySubjekt(subjekt);
+    }
+
+    removeMarksBySubjekt(subjekt) { //6. метод, удаляющий все оценки по переданному предмету
+        this.marks = this.marks.filter(mark => mark.subjekt !== subjekt);
+    }
+
+    printStudentInfo() { //метод вывода информации о студенте
+        console.log(`Студент: ${this.firstName} ${this.secondName}`);
+        console.log("Оценки:");
+        this.marks.forEach(mark => {
+            console.log(`${mark.subjekt}: ${mark.mark}`);
+        });
+    }
+}
+
+const student = new studentClass("Соня", "Кудрявцева");
+student.addMark("Математика", 5);
+student.addMark("Физика", 4);
+student.addMark("Химия", 3);
+
 let task = parseFloat(prompt("1 - Вывод средней оценки студента по всем приедметам, 2 - Вывод оценок по заданному предмету, 3 - Добавление оценки по предмету, 4 - Удаление всех оценок по предмету", 1))
 switch (task)
 {
     case 1:
-        Task1_1()
+        student.getAverageMark();
+        student.printStudentInfo();
         break
     case 2:
-        Task1_2()
+        student.alertSubjekt1();
+        student.printStudentInfo();
         break
     case 3:
-        addMark()
+        student.alertAddMark();
+        student.printStudentInfo();
         break
     case 4:
-        addMark()
+        student.alertSubjekt2();
+        student.printStudentInfo();
         break
 }
 
-function addMark (subjekt, mark){
-    let newMark = new marksClass(subjekt, mark);
-    this.marks.push(newMark);
-    printStudentInfo(studentVar);
-}
-
-function getAverageMark() {
-    if (this.marks.length === 0) {
-      return 0;
-    }
-    const totalMarks = this.marks.reduce((sum, mark) => sum + mark.mark, 0);
-    return totalMarks / this.marks.length;
-  }
-
-// marksClass.avgMarks = function  () {
-//     let sum = 0;
-//     for (let i=0; i<Object.keys(marksClass).length; i++) {
-//         sum += Object.keys(marksClass)[i];
-//         avg = sum/Object.keys(marksClass).length;
-//     }
-//     console.log(avg)
-//     console.log(sum)
-// }
-
-// console.log(marksClass.avgMarks())
-
-
-//тест коммит
 
