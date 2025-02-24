fd40a1e589850afa2b0d624547e41c4eaf91fc12
Кудрявцева Соня дом <kudryavtseva.sofi@yandex.ru>
2025-02-25 00:27:46 +0400

    Доделана ЛР 6

--- ЛР 6/index6.js
+++ ЛР 6/index6.js
@@ -29,10 +29,6 @@ Students = `{
                 {
                     "subject": "Физика",
                     "mark": 5
-                },
-                {
-                    "subject": "Химия",
-                    "mark": 4
                 }
             ]
         },
@@ -61,7 +57,7 @@ function printStudentsInfo () {
     console.log(JSON.stringify(Students))
 }
 
-function reverseStudents (data) {
+function reverseStudents (data) { //1. функция, которая возвращает переданный массив студентов, упорядоченный в обратном направлении.
     function sortStudentsReverse(students) {// Функция для сортировки элементов в обратном порядке
         students.students.reverse();// Сортируем массив студентов в обратном порядке
         return students;
@@ -71,7 +67,21 @@ function reverseStudents (data) {
     console.log(sortedJsonData);
 }
 
-function getStudentByMark (data){
+function getAllStudentsByMark(data) { //2. функция, которая находит и возвращает индексы всех студентов в массиве, у которых есть указанная оценка по указанному предмету
+    let subject = prompt("Введите предмет:");
+    let mark = parseInt(prompt("Введите оценку:"));
+    const indices = [];
+    data.students.forEach((student, index) => {
+        const hasMark = student.marks.some(m => m.subject === subject && m.mark === mark);
+        if (hasMark) {
+            indices.push(index);
+        }
+    });
+    console.log(indices);
+    return indices;
+}
+
+function getStudentByMark (data){//3. функция, которая находит и возвращает индекс первого студента в массиве, у которого есть указанная оценка по указанному предмету
     let subject = prompt("Введите предмет:");
     let mark = parseInt(prompt("Введите оценку:"));
     let res = data.students.findIndex(student =>
@@ -80,25 +90,44 @@ function getStudentByMark (data){
     console.log(res)
 }
 
+function studentsToMap (data) {//4. функция, которая превращает массив студентов в объект Map, где в качестве ключей используется строка, образованная конкатенацией имени и фамилии
+    const map = new Map();
+    data.students.forEach(student => {
+        const key = `${student.firstName} ${student.lastName}`;
+        map.set(key, student);
+    });
+    console.log(map)
+}
+
+function deleteStudents (data) {//5. функция, , которая удаляет из переданного массива всех студентов, у которых нет оценок по указанному предмету
+    let subject = prompt("Введите предмет:");
+    let res = data.students.filter(student =>
+        student.marks.some(mark => mark.subject === subject)
+    );
+    console.log(res)
+}
+
 const data = JSON.parse(Students);// Преобразуем JSON-строку в объект JavaScript
 
 while (true) {
-    let task = parseFloat(prompt("1 - Вывод информации о студентах, 2 - Вывод студентов в обратном направлении, 3 - Добавление оценки по предмету, 4 - Удаление всех оценок по предмету 5 - Выйти из программы", 1))
-    if (task === 5) break
+    let task = parseFloat(prompt("1 - массив студентов, упорядоченный в обратном направлении, 2 - Индексы всех студентов в массиве, у которых есть указанная оценка, 3 - Индекс первого студента в массиве, у которого есть указанная оценка 4 - Массив студентов в виде объекта map, 5 - Удаление студентов, у которых нет оценок по заданному предмету, 6 - Выход", 1))
+    if (task === 6) break
     switch (task)
     {
         case 1:
-            printStudentsInfo();
+            reverseStudents(data);
             break
         case 2:
-            reverseStudents(data);
+            getAllStudentsByMark(data);
             break
         case 3:
             getStudentByMark(data);
             break
         case 4:
-            student.alertsubject2();
-            student.printStudentInfo();
+            studentsToMap(data);
+            break
+        case 5:
+            deleteStudents(data);
             break
     }
 }
